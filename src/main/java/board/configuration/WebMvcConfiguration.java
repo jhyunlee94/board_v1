0af564f80333f99304ff044c24e2f4f70548991c
jhyunlee94 <jkwhd12@gmail.com>
2022-01-27 14:51:45 +0900

    0127 AOP LoggerAspect

--- src/main/java/board/configuration/WebMvcConfiguration.java
+++ src/main/java/board/configuration/WebMvcConfiguration.java
@@ -10,7 +10,7 @@ import board.interceptor.LoggerInterceptor;
 public class WebMvcConfiguration implements WebMvcConfigurer{
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new LoggerInterceptor());
+		registry.addInterceptor(new LoggerInterceptor()); // 인터셉터를 등록
 	}
 
 }
